efa25d5efb7219dba4919eeefc53093dacb1c13a
Collins O. Odhiambo <comon928@gmail.com>
2024-01-03 08:31:35 +0000

    add screts

--- app.py
+++ app.py
@@ -8,10 +8,14 @@ import time  # Import the time module for simulation
 import altair as alt
 import google.generativeai as genai
 
-with open('env') as f:
-    env_content = f.read()
 
-GEMINI_API_KEY = env_content.split('GEMINI_API_KEY=')[1].strip()
+GEMINI_API_KEY = st.secrets["db_username"]
+
+# with open('env') as f:
+#     env_content = f.read()
+
+# GEMINI_API_KEY = env_content.split('GEMINI_API_KEY=')[1].strip()
+
 #set genai api key
 genai.configure(api_key=GEMINI_API_KEY)
 
